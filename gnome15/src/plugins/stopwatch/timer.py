19b5fd8b2d9cd782a7728a030c5d5917b47a762e
Nuno Araujo <nuno.araujo@russo79.com>
2011-04-04 23:51:31 +0200

    Add stopwatch plugin
    This plugin gives the user two available timers.
    Each timer can be individually enabled.
    Each timer has a descritive label.
    Each timer can be configured to work in a stopwatch or countdown mode.
    When in countdown mode:
      - A timer can be configured to loop itself.
      - A notification is displayed each time the timer reaches zero.

---
+++ gnome15/src/plugins/stopwatch/timer.py
@@ -0,0 +1,90 @@
+#        +-----------------------------------------------------------------------------+
+#        | GPL                                                                         |
+#        +-----------------------------------------------------------------------------+
+#        | Copyright (c) Nuno Araujo <nuno.araujo@russo79.com>                         |
+#        |                                                                             |
+#        | This program is free software; you can redistribute it and/or               |
+#        | modify it under the terms of the GNU General Public License                 |
+#        | as published by the Free Software Foundation; either version 2              |
+#        | of the License, or (at your option) any later version.                      |
+#        |                                                                             |
+#        | This program is distributed in the hope that it will be useful,             |
+#        | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+#        | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+#        | GNU General Public License for more details.                                |
+#        |                                                                             |
+#        | You should have received a copy of the GNU General Public License           |
+#        | along with this program; if not, write to the Free Software                 |
+#        | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
+#        +-----------------------------------------------------------------------------+
+
+import datetime
+import subprocess
+
+class G15Timer():
+    TIMER_MODE_STOPWATCH = 0
+    TIMER_MODE_COUNTDOWN = 1
+
+    def __init__(self, plugin):
+        self.__enabled = False
+        self.__running = False
+        self.label = ""
+        self.mode = G15Timer.TIMER_MODE_STOPWATCH
+        self.initial_value = datetime.timedelta()
+        self.loop = False
+        self.reset()
+        self.plugin = plugin
+
+    def set_enabled(self, value):
+        if value != self.__enabled:
+            self.__enabled = value
+            self.pause()
+            self.reset()
+
+    def get_enabled(self):
+        return self.__enabled
+
+    def value(self):
+        rv = self.__value()
+        if self.mode == G15Timer.TIMER_MODE_COUNTDOWN:
+            # Handle timeout
+            if rv >= self.initial_value:
+                # Stop timer if not in loop mode
+                if not self.loop:
+                    self.pause()
+                self.reset()
+                rv = self.__value()
+                self.notify()
+            rv = self.initial_value - rv
+        return rv
+
+    def __value(self):
+        if not self.__running:
+            rv = self._last_value
+        else:
+            rv = datetime.datetime.now() - self._last_resume + self._last_value
+        return rv
+
+    def toggle(self):
+        if self.__running:
+            self.pause()
+        else:
+            self.resume()
+
+    def pause(self):
+        self._last_value = self.__value()
+        self._last_resume = datetime.datetime.now()
+        self.__running = False
+
+    def resume(self):
+        self._last_resume = datetime.datetime.now()
+        self.__running = True
+
+    def reset(self):
+        self._last_value = datetime.timedelta()
+        self._last_resume = datetime.datetime.now()
+
+    def notify(self):
+        p = subprocess.Popen(['notify-send',"--icon=gtk-info","Gnome15", "Timer '" + self.label + "' is over."])
+
+# vim:set ts=4 sw=4 et:
