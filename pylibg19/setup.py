b31e3427332b3e3b8b740186a2ff892a8f9aa74a
Brett Smith <tanktarta@blueyonder.co.uk>
2012-01-09 23:21:21 +0000

    Version bump.

--- pylibg19/setup.py
+++ pylibg19/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pylibg19',
-      version='0.0.3',
+      version='0.0.4',
       description='Python interface to the Logitech G19',
       requires='usb',
       author='MultiCoreNop,Brett Smith',
