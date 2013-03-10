achilleas-k/gnome15

commit eff57a485792b428b2f3ffcb04b951fc1cbfcbc4

0.9.2

gnome15-plugins/src/clock/clock.py
@@ -76,6 +76,10 @@ def show_preferences(parent, driver, gconf_client, gconf_key):
     display_date.set_active(gconf_client.get_bool(gconf_key + "/display_date"))
     display_date.connect("toggled", _changed, gconf_key + "/display_date", gconf_client)
     
+    use_24hr_format = widget_tree.get_object("TwentFourHourCheckbox")
+    use_24hr_format.set_active(gconf_client.get_bool(gconf_key + "/use_24hr_format"))
+    use_24hr_format.connect("toggled", _changed, gconf_key + "/use_24hr_format", gconf_client)
+    
     dialog.run()
     dialog.hide()
 
@@ -180,7 +184,7 @@ class G15Clock(g15plugin.G15Plugin):
         The watch function is used, as this will automatically track the monitor handles
         and clean them up when the plugin is deactivated
         '''        
-        self.watch(None, self._config_changed);
+        self.watch(None, self._config_changed)
     
     def deactivate(self):
         g15plugin.G15Plugin.deactivate(self)
@@ -304,6 +308,7 @@ class G15Clock(g15plugin.G15Plugin):
     def _load_configuration(self):
         self.display_date = self.gconf_client.get_bool(self.gconf_key + "/display_date")
         self.display_seconds = self.gconf_client.get_bool(self.gconf_key + "/display_seconds")
+        self.use_24hr_format = self.gconf_client.get_bool(self.gconf_key + "/use_24hr_format")
         
     def _redraw(self):
         '''
@@ -351,12 +356,12 @@ class G15Clock(g15plugin.G15Plugin):
         Get the details to display and place them as properties which are passed to
         the theme
         '''
-        time_format = "%H:%M"
-        properties["time_nosec"] = datetime.datetime.now().strftime(time_format)
-        if self.display_seconds:
-            time_format = "%H:%M:%S"
-        properties["time"] = datetime.datetime.now().strftime(time_format)            
+        now = datetime.datetime.now()
+        if self.use_24hr_format:
+            properties["time"] = g15locale.format_time_24hour(now, self.gconf_client, self.display_seconds)
+        else:
+            properties["time"] = g15locale.format_time(now, self.gconf_client, self.display_seconds)
         if self.display_date:
-            properties["date"] = datetime.datetime.now().strftime(locale.nl_langinfo(locale.D_FMT))
+            properties["date"] = g15locale.format_date(now, self.gconf_client)
             
         return properties

gnome15-indicator/g15-indicator
@@ -30,8 +30,6 @@ import sys
 import pygtk
 pygtk.require('2.0')
 import gtk
-import gnome
-import gnome.ui
 import os
 import appindicator
 import gconf

gnome15-plugins/configure.in
@@ -1,4 +1,4 @@
-AC_INIT(gnome15-plugins, 0.9.1)
+AC_INIT(gnome15-plugins, 0.9.2)
 AC_CONFIG_SRCDIR([src/cal/cal.py])
 AM_INIT_AUTOMAKE
 

gnome15-plugins/src/background/background.py
@@ -226,7 +226,7 @@ class G15Background():
             if active_profile is not None and active_profile.background is not None and active_profile.background != "":
                 self.bg_img = active_profile.background
         
-        if self.bg_img == None and  bg_type == "desktop":
+        if self.bg_img == None and bg_type == "desktop":
             # Get the current background the desktop is using if possible
             desktop_env = g15desktop.get_desktop()
             if desktop_env in [ "gnome", "gnome-shell" ]:

gnome15-plugins/src/cairo-clock/cairo-clock.glade
@@ -151,21 +151,6 @@
                           </packing>
                         </child>
                         <child>
-                          <object class="GtkCheckButton" id="TwentyFourHourCheckbox">
-                            <property name="label" translatable="yes">24hr Mode</property>
-                            <property name="visible">True</property>
-                            <property name="can_focus">True</property>
-                            <property name="receives_default">False</property>
-                            <property name="use_action_appearance">False</property>
-                            <property name="draw_indicator">True</property>
-                          </object>
-                          <packing>
-                            <property name="expand">True</property>
-                            <property name="fill">True</property>
-                            <property name="position">3</property>
-                          </packing>
-                        </child>
-                        <child>
                           <object class="GtkCheckButton" id="DisplayDigitalTimeCheckbox">
                             <property name="label" translatable="yes">Display Digital Time</property>
                             <property name="visible">True</property>
@@ -177,7 +162,7 @@
                           <packing>
                             <property name="expand">True</property>
                             <property name="fill">True</property>
-                            <property name="position">4</property>
+                            <property name="position">3</property>
                           </packing>
                         </child>
                         <child>
@@ -192,7 +177,38 @@
                           <packing>
                             <property name="expand">True</property>
                             <property name="fill">True</property>
+                            <property name="position">4</property>
+                          </packing>
+                        </child>
+                        <child>
+                          <object class="GtkCheckButton" id="TwentyFourHourCheckbox">
+                            <property name="label" translatable="yes">24hr Analogue Time (requires 24hr theme)</property>
+                            <property name="visible">True</property>
+                            <property name="can_focus">True</property>
+                            <property name="receives_default">False</property>
+                            <property name="use_action_appearance">False</property>
+                            <property name="draw_indicator">True</property>
+                          </object>
+                          <packing>
+                            <property name="expand">True</property>
+                            <property name="fill">True</property>
                             <property name="position">5</property>
+                          </packing>
+                        </child>
+                        <child>
+                          <object class="GtkCheckButton" id="TwentyFourHourDigitalCheckbox">
+                            <property name="label" translatable="yes">24hr Digital Time</property>
+                            <property name="visible">True</property>
+                            <property name="can_focus">True</property>
+                            <property name="receives_default">False</property>
+                            <property name="use_action_appearance">False</property>
+                            <property name="xalign">0.54000002145767212</property>
+                            <property name="draw_indicator">True</property>
+                          </object>
+                          <packing>
+                            <property name="expand">True</property>
+                            <property name="fill">True</property>
+                            <property name="position">6</property>
                           </packing>
                         </child>
                       </object>

gnome15-plugins/src/cairo-clock/cairo-clock.py
@@ -100,6 +100,7 @@ class G15CairoClockPreferences():
         g15util.configure_checkbox_from_gconf(gconf_client, "%s/display_digital_time" % gconf_key, "DisplayDigitalTimeCheckbox", True, widget_tree)
         g15util.configure_checkbox_from_gconf(gconf_client, "%s/display_year" % gconf_key, "DisplayYearCheckbox", True, widget_tree)
         g15util.configure_checkbox_from_gconf(gconf_client, "%s/second_sweep" % gconf_key, "SecondSweep", False, widget_tree)
+        g15util.configure_checkbox_from_gconf(gconf_client, "%s/twenty_four_hour_digital" % gconf_key, "TwentyFourHourDigitalCheckbox", True, widget_tree)
     
         e = gconf_client.get(gconf_key + "/theme")
         theme_name = "default"
@@ -183,6 +184,10 @@ class G15CairoClock(g15plugin.G15RefreshingPlugin):
         self.display_year = g15util.get_bool_or_default(self.gconf_client, "%s/display_year" % self.gconf_key, True)
         self.display_digital_time = g15util.get_bool_or_default(self.gconf_client, "%s/display_digital_time" % self.gconf_key, True)
         self.second_sweep = g15util.get_bool_or_default(self.gconf_client, "%s/second_sweep" % self.gconf_key, False)
+        self.twenty_four_hour = g15util.get_bool_or_default(self.gconf_client, "%s/twenty_four_hour" % self.gconf_key, False)
+        self.twenty_four_hour_digital = g15util.get_bool_or_default(self.gconf_client, "%s/twenty_four_hour_digital" % self.gconf_key, True)
+        
+        self.gconf_client.get_bool(self.gconf_key + "/twenty_four_hour")
         
         self.svg_size = None
         self.width = self.screen.width
@@ -298,10 +303,10 @@ class G15CairoClock(g15plugin.G15RefreshingPlugin):
     def _get_time_text(self, display_seconds = None):
         if display_seconds == None:
             display_seconds = self.display_seconds
-        time_format = "%H:%M"
-        if display_seconds:
-            time_format = "%H:%M:%S"
-        return datetime.datetime.now().strftime(time_format)
+        if self.twenty_four_hour_digital:
+            return g15locale.format_time_24hour(datetime.datetime.now(), self.gconf_client, display_seconds)
+        else:
+            return g15locale.format_time(datetime.datetime.now(), self.gconf_client, display_seconds)
     
     def _get_date_text(self):
         if self.display_year:
@@ -399,7 +404,7 @@ class G15CairoClock(g15plugin.G15RefreshingPlugin):
             s_deg = now.second * 6
         m_deg = now.minute * 6 + ( now.second * ( 6.0 / 60.0 ) )
         
-        if self.gconf_client.get_bool(self.gconf_key + "/twenty_four_hour"):
+        if self.twenty_four_hour:
             h_deg = float(now.hour) * 15.0 + (  float ( now.minute * 0.25 ) )
         else:
             h_deg = float( now.hour % 12 ) * 30.0 + (  float ( now.minute * 0.5 ) )

gnome15-plugins/src/cal/Makefile.am
@@ -1,6 +1,7 @@
 SUBDIRS = default
 plugindir = $(datadir)/gnome15/plugins/cal
 plugin_DATA = cal.py \
+	cal.glade \
 	bell.gif
 
 EXTRA_DIST =  			\

gnome15-plugins/src/cal/cal.glade
@@ -1,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+  <requires lib="gtk+" version="2.16"/>
+  <!-- interface-naming-policy project-wide -->
+  <object class="GtkWindow" id="window1">
+    <property name="can_focus">False</property>
+    <child>
+      <object class="GtkVBox" id="OptionPanel">
+        <property name="visible">True</property>
+        <property name="can_focus">False</property>
+        <child>
+          <object class="GtkCheckButton" id="TwentyFourHourTimes">
+            <property name="label" translatable="yes">Use 24hr format for times</property>
+            <property name="visible">True</property>
+            <property name="can_focus">True</property>
+            <property name="receives_default">False</property>
+            <property name="use_action_appearance">False</property>
+            <property name="draw_indicator">True</property>
+          </object>
+          <packing>
+            <property name="expand">False</property>
+            <property name="fill">True</property>
+            <property name="position">0</property>
+          </packing>
+        </child>
+        <child>
+          <placeholder/>
+        </child>
+      </object>
+    </child>
+  </object>
+</interface>

commit 248605aacd54d55f1da970ab72059718700a102f

clock plugin: fix the thumbnail display on 1 bpp displays
A regression was created with commit eff57a485792b428b2f3ffcb04b951fc1cbfcbc4
The time_nosec used to display the thumbnail in 1bpp displays was no
longuer set.

Fixes #156

gnome15-plugins/src/clock/clock.py
@@ -1,7 +1,8 @@
 #        +-----------------------------------------------------------------------------+
 #        | GPL                                                                         |
 #        +-----------------------------------------------------------------------------+
-#        | Copyright (c) Brett Smith <tanktarta@blueyonder.co.uk>                      |
+#        | Copyright (c) 2010-2012 Brett Smith <tanktarta@blueyonder.co.uk>            |
+#        | Copyright © 2013 Nuno Araujo <nuno.araujo@russo79.com>                      |
 #        |                                                                             |
 #        | This program is free software; you can redistribute it and/or               |
 #        | modify it under the terms of the GNU General Public License                 |
@@ -228,7 +229,9 @@ class G15Clock(g15plugin.G15Plugin):
             properties = self._get_properties()
             # Don't display the date or seconds on mono displays, not enough room as it is
             if self.screen.driver.get_bpp() == 1:
-                text = properties["time_nosec"]
+                text = properties["time"]
+                if self.display_seconds:
+                    text = text[:-3]
                 font_size = 8
                 factor = 2
                 font_name = g15globals.fixed_size_font_name