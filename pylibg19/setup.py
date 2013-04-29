4fb5695774c1400709362c7c0441cc4bb86e9495
Nuno Araujo <nuno.araujo@russo79.com>
2013-04-29 05:07:48 +0200

    Update version

--- pylibg19/setup.py
+++ pylibg19/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pylibg19',
-      version='0.0.5',
+      version='0.0.6',
       description='Python interface to the Logitech G19',
       requires='usb',
       author='MultiCoreNop,Brett Smith',
