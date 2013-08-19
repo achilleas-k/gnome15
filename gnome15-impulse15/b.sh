51c5929a64cee5b2b4cba12003b6598983e41f79
Nuno Araujo <nuno.araujo@russo79.com>
2013-08-19 12:25:01 +0200

    move gnome15-impulse15 into gnome15 folder
    refs #171

--- gnome15-impulse15/b.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-version=0.0.13
-
-autoreconf -f && ./configure && make && make dist && cp gnome15-impulse15-${version}.tar.gz ~/Workspaces/home\:tanktarta\:gnome15-unstable/gnome15-impulse15/ 
