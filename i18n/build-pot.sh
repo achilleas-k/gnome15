c48ca0285dda7acb94944bdba2f04ccaf80442a4
Nuno Araujo <nuno.araujo@russo79.com>
2013-08-24 18:56:25 +0200

    fix a path used by the script used for extractiong strings for translations
    refs #171

--- i18n/build-pot.sh
+++ i18n/build-pot.sh
@@ -3,8 +3,8 @@
 cd $(dirname $0)
 
 # Python
-xgettext --from-code=UTF-8 --language=Python --keyword=N_ --keyword=_ --output=gnome15-drivers.pot ./src/gnome15/drivers/*.py
-xgettext --from-code=UTF-8 --language=Python --keyword=N_ --keyword=_ --output=gnome15.pot ./src/gnome15/*.py
+xgettext --from-code=UTF-8 --language=Python --keyword=N_ --keyword=_ --output=gnome15-drivers.pot ../src/gnome15/drivers/*.py
+xgettext --from-code=UTF-8 --language=Python --keyword=N_ --keyword=_ --output=gnome15.pot ../src/gnome15/*.py
 
 # Glade
 for i in ../data/glade/*.glade; do
