15a67e7e0bb12b286a2c0190187d3035351660ea
Nuno Araujo <nuno.araujo@russo79.com>
2013-08-27 09:55:50 +0200

    update the license
    Gnome15 is licensed under the GPL3+ license, but also contains code
    from other projects, some of them under the MIT license.
    
    The headers of the source code files didn't reflect these and were now
    updated.
    
    Also, I tried to lookup the original code authors and have updated the
    AUTHORS file accordingly.
    
    refs #219

--- src/plugins/cairo-clock/cairo-clock.py
+++ src/plugins/cairo-clock/cairo-clock.py
@@ -1,22 +1,18 @@
-#        +-----------------------------------------------------------------------------+
-#        | GPL                                                                         |
-#        +-----------------------------------------------------------------------------+
-#        | Copyright (c) Brett Smith <tanktarta@blueyonder.co.uk>                      |
-#        |                                                                             |
-#        | This program is free software; you can redistribute it and/or               |
-#        | modify it under the terms of the GNU General Public License                 |
-#        | as published by the Free Software Foundation; either version 2              |
-#        | of the License, or (at your option) any later version.                      |
-#        |                                                                             |
-#        | This program is distributed in the hope that it will be useful,             |
-#        | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
-#        | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
-#        | GNU General Public License for more details.                                |
-#        |                                                                             |
-#        | You should have received a copy of the GNU General Public License           |
-#        | along with this program; if not, write to the Free Software                 |
-#        | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. |
-#        +-----------------------------------------------------------------------------+
+#  Gnome15 - Suite of tools for the Logitech G series keyboards and headsets
+#  Copyright (C) 2010 Brett Smith <tanktarta@blueyonder.co.uk>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  
 import gnome15.g15locale as g15locale
 _ = g15locale.get_translation("cairo-clock", modfile = __file__).ugettext
