4ea7201da6e0444b1ac494543a39fef8b3ccff3a
Nuno Araujo <nuno.araujo@russo79.com>
2013-11-05 18:40:26 +0100

    trafficstats: fix a catch clause
    refs #269

--- src/plugins/trafficstats/trafficstats.py
+++ src/plugins/trafficstats/trafficstats.py
@@ -34,7 +34,7 @@ import logging
 logger=logging.getLogger(__name__)
 try:
     import gtop
-except:
+except Exception as e:
     logger.debug("Could not import gtop. Falling back to g15top", exc_info = e)
     # API compatible work around for Ubuntu 12.10
     import gnome15.g15top as gtop
